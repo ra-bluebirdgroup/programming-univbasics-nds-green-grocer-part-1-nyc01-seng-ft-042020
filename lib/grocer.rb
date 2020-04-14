d9a797abf7b256d8a65d777ee06403542838febf
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-14 21:12:12 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -31,7 +31,7 @@ cart = [
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
-new_cart = []  
+new_cart = []
 cart.each do product_hash
 find_item_by_name_in_collection(name, new_cart)
  p new_cart
