48ea03a6578acc6011b6c26669fd84b109fb2758
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-14 22:10:45 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -32,18 +32,26 @@ cart = [
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
 new_cart = []
-
-cart.clone.each do |product_hash|
-  product_hash.select { |key, value|
-    if  !find_item_by_name_in_collection(value, new_cart)
-      new_cart.each do |new_product_hash|
-      product_hash[:count] = 1
-      new_cart << product_hash
-        end
-    end
-    }
-
+cart.each do |product_hash|
+  product_hash.select { |key, value| 
+    
+if !find_item_by_name_in_collection(value, new_cart)
+    new_cart << product_hash  
+ else 
+    new_cart.each do |new_product_hash| 
+      product_hash[count]
+   
 end
+
+}
+
+end 
+
+
+new_cart.each do |new_product_hash|
+      new_product_hash.merge!(count: 1)
+    end  
+
 
 p new_cart
 
