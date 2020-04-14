f97374bdbb65196799ec4c271dcb9e5257b23364
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-14 21:40:17 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -37,7 +37,7 @@ cart.each do |product_hash|
 if !find_item_by_name_in_collection(value, new_cart)
     product_hash.merge!(count: 1)
     new_cart << product_hash
-end  
+end
    product_hash.merge!(count: 1)
 
 end
