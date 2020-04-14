1687e58c64a3400181faf9d953e2843768586686
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-14 22:08:45 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -38,10 +38,12 @@ cart.clone.each do |product_hash|
     if find_item_by_name_in_collection(value, new_cart)
     new_cart.each do |new_product_hash|
     new_product_hash[:count] += 1
+       end
     elsif !find_item_by_name_in_collection(value, new_cart)
+      new_cart.each do |new_product_hash|
       product_hash[:count] = 1
       new_cart << product_hash
-        end
+
     end
     }
 
