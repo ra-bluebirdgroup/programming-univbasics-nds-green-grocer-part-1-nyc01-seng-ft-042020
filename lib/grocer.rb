fc52b7dc32f35354539051219ee2179c966735a8
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-14 22:42:33 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -34,8 +34,8 @@ cart = [
 new_cart = []
 cart.each do |product_hash|
   product_hash.clone.select { |key, value|
-
-if !find_item_by_name_in_collection(key, new_cart)
+   new_cart.each do new_product_hash
+if !find_item_by_name_in_collection(key, new_product_hash) 
     product_hash[:count] = 1
     new_cart << product_hash
   else
