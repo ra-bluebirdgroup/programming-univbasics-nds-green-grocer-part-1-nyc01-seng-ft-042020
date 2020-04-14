96a9ffc32a8fde65156dfe6b021a16e404339968
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-14 20:25:47 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,15 +20,21 @@ end
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
-hash = []
- cart.each do |item_hash|
-   item_hash.each do |name, price_hash|
-     if hash[name].nil?
-       hash[name] = price_hash.merge({:count => 1})
-     else
-       hash[name][:count] += 1
-     end
-   end
- end
- hash
+new_cart = []
+cart.each do |item_hash|
+  item_hash.each do |key, value|
+     find_item_by_name_in_collection(key, new_cart) ?  
+      new_cart.each do product_hash 
+        if product_hash.include?(key) 
+          product_hash[:count] += 1
+        end 
+        :  
+        item_hash[:count] = 1
+        new_cart << item_hash 
+
+      end 
+    end 
+  end 
+          
+ new_cart
 end
