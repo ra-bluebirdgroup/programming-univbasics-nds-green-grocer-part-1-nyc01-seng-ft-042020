7a6747b1608ec1a420d41a222cea314e446ccacb
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-15 01:32:41 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -39,8 +39,8 @@ if !find_item_by_name_in_collection(value, new_cart)
     product_hash[:count] = 1
     new_cart << product_hash
 end
-if find_item_by_name_in_collection(value, new_cart)
-     new_cart[product_hash][:count] += 1
+if find_item_by_name_in_collection(value, new_cart) and new_cart[product_hash].has_value?(value)
+  new_cart[product_hash][:count += 1]
  end
 
 }
