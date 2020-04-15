ea84620a34ffc8d5a97fc723c4cb3e06207b0455
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-15 03:37:56 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -26,7 +26,7 @@ cart = [
   #
   # Consult README for inputs and outputs
 
-# def consolidate_cart(cart)
+ def consolidate_cart(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
@@ -41,4 +41,4 @@ cart = [
     end
   end
   p new_cart
- # end
+ end
