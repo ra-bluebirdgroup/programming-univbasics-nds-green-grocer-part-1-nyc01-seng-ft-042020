fabbf6a6226923910bae0be444c914163902d9cb
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-15 00:31:47 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -38,10 +38,10 @@ if find_item_by_name_in_collection(key, new_cart)
   new_cart.each do |new_product_hash|
     if new_product_hash.has_key?(key)
        new_cart[new_product_hash][:count] += 1
-     end 
+     end
+   end
   else
-    new_cart.each do |new_product_hash|
-      new_product_hash[:count] = 1
+       product_hash[:count] = 1
        new_cart << product_hash
  end
 }
