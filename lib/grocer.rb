3baa5ef4bc6ed13fac26e246d155924485e0309e
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-15 04:09:32 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,6 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
   collection.each do |product_hash|
-    product_hash.select{|key, value|
+    product_hash.select{ |key, value|
          if value.to_s == name
            return product_hash
          end
@@ -11,39 +10,18 @@ nil
 
 end
 
-cart = [
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "KALE", :price => 3.00, :clearance => false},
-  {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
-  {:item => "ALMONDS", :price => 9.00, :clearance => false},
-  {:item => "TEMPEH", :price => 3.00, :clearance => true},
-  {:item => "CHEESE", :price => 6.50, :clearance => false},
-  {:item => "BEER", :price => 13.00, :clearance => false},
-  {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
-  {:item => "BEETS", :price => 2.50, :clearance => false},
-  {:item => "SOY MILK", :price => 4.50, :clearance => true}
-]
-  #
-  # Consult README for inputs and outputs
-
- def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+def consolidate_cart(cart)
   new_cart = []
-  cart.each do |product_hash|
-  if !find_item_by_name_in_collection(product_hash[:item], new_cart)
-    product_hash[:count] = 1
-    new_cart <<  product_hash
 
-  else
-    new_cart.each do |new_product_hash|
-    new_product_hash[:count] += 1
-       end
+  cart.each do |product_hash|
+    if !find_item_by_name_in_collection(product_hash[:item], new_cart)
+      product_hash[:count] = 1
+      new_cart <<  product_hash
+    else
+      new_cart.each do |new_product_hash|
+      new_product_hash[:count] += 1 end
     end
   end
 
-new_cart
+ new_cart
 end
