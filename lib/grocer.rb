52b12d5d57b37ec03660f0376683097086416d77
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-15 03:17:47 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -33,9 +33,8 @@ cart = [
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   new_cart = []
   cart.each do |product_hash|
-    product_hash.select { |k, v|
-     if !find_item_by_name_in_collection(v, new_cart)
-       product_hash[:count] = 1
+    if !find_item_by_name_in_collection(product_hash[0], new_cart)
+       product_hash[:counter] =1
        new_cart << product_hash
        new_cart.each do |new_product_hash|
         if new_product_hash.has_value?(product[0])
