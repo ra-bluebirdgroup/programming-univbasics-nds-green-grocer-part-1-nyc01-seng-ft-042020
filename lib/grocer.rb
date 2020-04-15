1947a83288314745d8cfb277c4fc720b55b6d547
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-15 00:33:18 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -10,18 +10,19 @@ def find_item_by_name_in_collection(name, collection)
 puts "We're sorry, item not found"
 
 end
-# cart = [
-#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
-#   {:item => "KALE", :price => 3.00, :clearance => false},
-#   {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
-#   {:item => "ALMONDS", :price => 9.00, :clearance => false},
-#   {:item => "TEMPEH", :price => 3.00, :clearance => true},
-#   {:item => "CHEESE", :price => 6.50, :clearance => false},
-#   {:item => "BEER", :price => 13.00, :clearance => false},
-#   {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
-#   {:item => "BEETS", :price => 2.50, :clearance => false},
-#   {:item => "SOY MILK", :price => 4.50, :clearance => true}
-# ]
+
+cart = [
+  {:item => "AVOCADO", :price => 3.00, :clearance => true},
+  {:item => "KALE", :price => 3.00, :clearance => false},
+  {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+  {:item => "ALMONDS", :price => 9.00, :clearance => false},
+  {:item => "TEMPEH", :price => 3.00, :clearance => true},
+  {:item => "CHEESE", :price => 6.50, :clearance => false},
+  {:item => "BEER", :price => 13.00, :clearance => false},
+  {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+  {:item => "BEETS", :price => 2.50, :clearance => false},
+  {:item => "SOY MILK", :price => 4.50, :clearance => true}
+]
   #
   # Consult README for inputs and outputs
 
